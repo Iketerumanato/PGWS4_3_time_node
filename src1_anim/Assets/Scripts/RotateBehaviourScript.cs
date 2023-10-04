b6e050bf505b8d06199763964662ddba03473de2
teru <74332407+Iketerumanato@users.noreply.github.com>
2023-10-04 16:17:18 +0900

    AddShaderRotate
    object自体を回転させたものとshaderの読み込み位置を変えて回転させたもの

--- src1_anim/Assets/Scripts/RotateBehaviourScript.cs
+++ src1_anim/Assets/Scripts/RotateBehaviourScript.cs
@@ -6,5 +6,7 @@ public class RotateBehaviourScript : MonoBehaviour
 {
     void Update()
     {
+        float rota = 360.0f * Time.deltaTime / 10.0f;
+        this.transform.Rotate(0f, rota, 0f);
     }
 }
